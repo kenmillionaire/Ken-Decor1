bf23eb5e19ac8cc487fa818712e08105e3631ee7
kenmillionaire <osobsim@gmail.com>
2023-02-21 00:10:06 -0400

    flowbite installed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Nav from "../Components/Nav";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Nav />
     </>
   );
 }
