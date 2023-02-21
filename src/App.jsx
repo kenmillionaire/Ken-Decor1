167f3a52c73b088477f9a4788ade3b664c019946
kenmillionaire <osobsim@gmail.com>
2023-02-20 21:13:24 -0400

    Update App.jsx and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return;
+  <>
+    <Home />
+  </>;
 }
 
 export default App;
